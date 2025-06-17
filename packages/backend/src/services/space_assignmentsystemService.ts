6281794db37cf407435c4dfb5e2b1ec947c0abef
navaShechter <n0556775395@gmail.com>
2025-06-17 13:38:42 +0300

    הוספת קונטרולר סרביס למערכת סביבת עבודה עם לוח שנה

---
+++ packages/backend/src/services/space_assignmentsystemService.ts
@@ -0,0 +1,14 @@
+import {space_assignmentsystem} from '../models/';
+//פונקציה המקצה חדר למשתמש תוך בדיקה שאין התנגשויות
+async function assignCustomerToWorkspace(customerId:any) {
+}
+//פונקציה המעבירה לקוח לחדר אחר תוך כדי בדיקה שאין התנגשויות
+async function unassignCustomerFromWorkspace(customerId:any,roomid:any) {
+}
+//פונקציה המחירה את כל ההקצאות בעבר ללקוח
+async function getAssignmentHistoryForCustomer(customerId:any) {
+}
+//פונקציה המקצה ללקוח חדרים מרובים
+async function assignCustomerToWorkspaces(workspaceid:any,customerId:any) {
+    
+}
