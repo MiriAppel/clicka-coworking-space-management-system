c43ada2daadc37347e2be8947b380f53d07ae9c2
Malky Lev <malkylev939@gmail.com>
2025-06-18 12:13:44 +0300

    type dir

--- packages/backend/src/controllers/contract.controller.ts
+++ packages/backend/src/controllers/contract.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import {contractService} from '../services/contract.service'
-import { AddContractDocumentRequest, Contract } from '../types/customer';
-import { ID } from '../types/core';
+import { AddContractDocumentRequest, Contract } from '../../../../types/customer';
+import { ID } from '../../../../types/core';
 import { ContractModel } from '../models/contract.model';
 
 const serviceContract = new contractService();
