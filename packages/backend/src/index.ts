638efcdd987f694cd68691dee4ec10836794d8ef
Lea Shorr <L0548544962@gmail.com>
2025-06-19 11:22:28 +0300

    added app and index

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,14 @@
+
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+console.log('process.env.GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Open your browser and go to http://localhost:${PORT}`);
+});
