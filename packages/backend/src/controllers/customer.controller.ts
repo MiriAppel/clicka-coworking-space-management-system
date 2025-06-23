67e102bcecc994a6af8b0e6cd73d7676ec1b76b6
miri bar lev <m0534198438@gmail.com>
2025-06-23 12:08:28 +0300

    fix postCustomer in controller

--- packages/backend/src/controllers/customer.controller.ts
+++ packages/backend/src/controllers/customer.controller.ts
@@ -1,6 +1,9 @@
 
 import { Request, Response } from 'express';
 import { customerService } from '../services/customer.service';
+import { CreateCustomerRequest } from '../../../../types/customer';
+import { ID } from '../../../../types/core';
+import { PaymentMethodType } from '../../../../types/billing';
 
 
 const serviceCustomer = new customerService();
@@ -17,10 +20,27 @@ export const getAllCustomers = async (req: Request, res: Response) => {
 }
 
 export const postCustomer = async (req: Request, res: Response) => {
+  try {
+    const {
+      newCustomer,
+      leadId,
+      paymentMethodsType,
+      businessName,
+    }: {
+      newCustomer: CreateCustomerRequest;
+      leadId: ID;
+      paymentMethodsType: PaymentMethodType;
+      businessName: string;
+    } = req.body;
 
-    try {
-        const customers = await serviceCustomer.convertLeadToCustomer(req.body)
-        res.status(200).json(customers);
+    const customer = await serviceCustomer.convertLeadToCustomer(
+      newCustomer,
+      leadId,
+      paymentMethodsType,
+      businessName
+    );
+
+    res.status(200).json(customer);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching customers', error });
     }
