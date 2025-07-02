aa3e6bd8df3ef041f64beadb064b29244703764e
Nechama <ny545185@gmail.com>
2025-07-02 16:00:15 +0300

    baseMap

--- packages/frontend/src/routing.jsx
+++ packages/frontend/src/routing.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import App from "./App"
-import { WorkspaceMap } from "./MainMenu/Workspace/Components/workspaceMap"
 import { Billing } from "./MainMenu/Billing/Components/billing"
 import { LeadAndCustomerRouting } from "./MainMenu/LeadAndCustomer/Components/LeadAndCustomerRouting"
+import { WorkspaceMap } from "./MainMenu/Workspace/Components/workspaceMap"
 
 
 export const routing = () => {
@@ -10,7 +10,7 @@ export const routing = () => {
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="leadAndCustomer/*" element={<LeadAndCustomerRouting />} />
-            <Route path="workspaceMap" element={< WorkspaceMap />} />
+            <Route path="workspaceMap" element={<WorkspaceMap />} />
             <Route path="billing" element={< Billing />} />
         </Routes>
     </>
