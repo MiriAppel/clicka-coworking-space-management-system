904c85900e0ccc9c3d18690bddce9a6e28772be2
Nechama <ny545185@gmail.com>
2025-07-03 10:48:59 +0300

    save-map-2

--- packages/backend/src/services/translation.service.ts
+++ packages/backend/src/services/translation.service.ts
@@ -10,6 +10,8 @@ dotenv.config();
 // יצירת לקוח Supabase
 const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseAnonKey = process.env.SUPABASE_SERVICE_KEY || '';
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY);
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 // בדיקת תקינות שפה
