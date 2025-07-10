797b3c1893ac7e5dc8ec0cf1e543f5118edc0f34
leale <a0527121791@gmail.com>
2025-07-10 10:23:26 +0300

    both part

--- packages/frontend/src/reportWebVitals.ts
+++ packages/frontend/src/reportWebVitals.ts
@@ -1,6 +1,6 @@
-// // import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from 'web-vitals';
 
-// // const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
 // //   // if (onPerfEntry && onPerfEntry instanceof Function) {
 // //   //   import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
 // //   //     getCLS(onPerfEntry);
@@ -10,6 +10,6 @@
 // //   //     getTTFB(onPerfEntry);
 // //   //   });
 // //   // }
-// // };
+};
 
-// export default reportWebVitals;
+export default reportWebVitals;
