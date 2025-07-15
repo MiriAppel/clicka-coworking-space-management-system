ec6e8a8da1bb23fd68ce891a570b590861c7ae15
LeaLiv <leahl11730@gmail.com>
2025-07-15 12:59:29 +0300

    fix formatting

--- packages/frontend/src/Services/Axios.tsx
+++ packages/frontend/src/Services/Axios.tsx
@@ -4,11 +4,11 @@ const server = process.env.REACT_APP_API_URL;
 
 //חיבור לשרת הAPI של המשתמשים
 export const axiosInstance = axios.create({
-    baseURL: server,
-    headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    
-    export default axiosInstance;
-    // withCredentials: true,
+  baseURL: server,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export default axiosInstance;
