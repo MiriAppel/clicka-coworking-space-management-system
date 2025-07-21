7c1a8a76c0dc13d71e2fb854fd03d7d74dc7cf63
tova ginzburg <t0583214246@gmail.com>
2025-07-21 12:01:39 +0300

    Storybook and AuditLogTable

--- packages/frontend/src/Service/Axios.tsx
+++ packages/frontend/src/Service/Axios.tsx
@@ -5,5 +5,10 @@ const server = process.env.REACT_APP_API_URL;
 //חיבור לשרת הAPI של המשתמשים
 export const axiosInstance = axios.create({
     baseURL: server,
-    withCredentials: true,
-})
+    headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true, // Enable sending cookies with requests
+    });
+    
+    export default axiosInstance;
