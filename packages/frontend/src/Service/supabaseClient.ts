df135866e900a3de51ea4d57684b572880be3a58
tova ginzburg <t0583214246@gmail.com>
2025-07-21 13:03:27 +0300

    update

--- packages/frontend/src/Service/supabaseClient.ts
+++ packages/frontend/src/Service/supabaseClient.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { createClient } from '@supabase/supabase-js';
-
-console.log('SUPABASE_URL:', process.env.REACT_APP_SUPABASE_URL);
-console.log('SUPABASE_KEY:', process.env.REACT_APP_SUPABASE_KEY);
-
-export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL!,
-  process.env.REACT_APP_SUPABASE_KEY!
-  
-);
-=======
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
@@ -19,4 +7,3 @@ console.log("SUPABASE_URL:", supabaseUrl);
 console.log("SUPABASE_ANON_KEY:", supabaseAnonKey);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
->>>>>>> origin
