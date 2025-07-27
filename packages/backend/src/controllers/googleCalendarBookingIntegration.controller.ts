2c97c4155ec9f28e4ece35f5a3a8b5013725ce85
leale <a0527121791@gmail.com>
2025-07-27 16:02:40 +0300

    install types

--- packages/backend/src/controllers/googleCalendarBookingIntegration.controller.ts
+++ packages/backend/src/controllers/googleCalendarBookingIntegration.controller.ts
@@ -117,12 +117,12 @@ export async function deleteCalendarSync(req: Request, res: Response) {
 export const getCalendarByRoom = async (req: Request, res: Response) => {
     try {
         const roomId: ID = req.params.roomId;
-        const calendar = await CalendarService.getCalendarByRoom(roomId);
-        if (calendar) {
-            res.status(200).json(calendar);
-        } else {
-            res.status(404).json({ message: 'Calendar not found for the specified room' });
-        }
+        // const calendar = await CalendarService.getCalendarByRoom(roomId);
+        // if (calendar) {
+        //     res.status(200).json(calendar);
+        // } else {
+        //     res.status(404).json({ message: 'Calendar not found for the specified room' });
+        // }
     } catch (error) {
         console.error('Error getting calendar by room:', error);
         res.status(500).json({ message: 'Failed to get calendar by room', error: error });
